5d8113895dd5e1cf8e3107c7acb8f7521e203f84
xuhaowen <502300247@qq.com>
2023-04-19 11:14:52 +0800

    change1

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/education')
+def activities():
+    return render_template('education.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
